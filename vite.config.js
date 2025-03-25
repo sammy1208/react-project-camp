86066bcd94f0890099b00ffc33acda34933d1972
sammy1208 <34804688+sammy1208@users.noreply.github.com>
2025-03-25 15:03:29 +0800

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: import.meta.env.MODE === 'production' ? '/react-project-camp/' : '/',
-  plugins: [react()],
-})
+// Use default base, and override via CLI or env file
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    base: isProduction ? '/react-project-camp/' : '/',
+    plugins: [react()],
+  };
+});
