90e3f118df58dc6df72ea2ed1b11fea626a4f662
sammy1208 <34804688+sammy1208@users.noreply.github.com>
2025-03-25 14:57:57 +0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-project-camp/' : '/',
+  base: import.meta.env.MODE === 'production' ? '/react-project-camp/' : '/',
   plugins: [react()],
 })
